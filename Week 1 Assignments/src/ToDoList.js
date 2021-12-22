e07590418ab604f89473645d994ae76f56fa9be9
krosentrater <87054047+krosentrater@users.noreply.github.com>
2021-12-22 08:14:44 -0800

    Week 1 Assignment 1
    ToDoList.js file for React.

---
+++ Week 1 Assignments/src/ToDoList.js
@@ -0,0 +1,26 @@
+
+
+function ToDoList() {
+
+    return (
+        <>
+            <h1>To Do List:</h1>
+                <input type='checkbox'></input>
+                <label>Go to grocery store</label>
+                <br></br>
+
+                <input type='checkbox'></input>
+                <label>Change oil</label>
+                <br></br>
+
+                <input type='checkbox'></input>
+                <label>Finish laundry</label>
+                <br></br>
+
+                <input type='checkbox'></input>
+                <label>Begin homework assignment</label>
+        </>
+    );
+}
+
+export default ToDoList;
